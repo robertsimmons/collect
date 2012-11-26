7d1bb0ff4fb623941a61ce58e9e1abe57da90e75
Robert Simmons <robert.simmons.tn@gmail.com>
2012-11-26 11:15:22 -0600

    Little JS refactoring.

---
+++ Collect.Web/Shared/Scripts/customlivevalidation.js
@@ -0,0 +1,16 @@
+﻿function CustomLiveValidation(elementId, prettyName) {
+
+	var validation = new LiveValidation(elementId, {
+		onValid: function () {
+			this.insertMessage(this.createMessageSpan());
+			this.addFieldClass();
+			$("#" + this.elementId).next("span.LV_valid").html("&#10004;");
+		}
+	});
+	validation.elementId = elementId;
+	validation.prettyName = prettyName
+
+	validation.add(Validate.Presence, { failureMessage: prettyName + " must be provided." });
+
+	return validation;
+}
